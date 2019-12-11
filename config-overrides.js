828cc8132ae76c7d2e6b7e26af7a3d867f490291
胡本绿 <benlv.hu@dianrong.com>
2019-12-11 23:56:46 +0800

    issue 9

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,59 @@
-const multipleEntry = require("react-app-rewire-multiple-entry")([
+const fs = require('fs');
+const path = require('path');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const moduleFileExtensions = [
+  'web.mjs',
+  'mjs',
+  'web.js',
+  'js',
+  'web.ts',
+  'ts',
+  'web.tsx',
+  'tsx',
+  'json',
+  'web.jsx',
+  'jsx',
+];
+const resolveModule = (resolveFn, filePath) => {
+  const extension = moduleFileExtensions.find(extension =>
+    fs.existsSync(resolveFn(`${filePath}.${extension}`))
+  );
+
+  if (extension) {
+    return resolveFn(`${filePath}.${extension}`);
+  }
+
+  return resolveFn(`${filePath}.js`);
+};
+
+const htmlFiles = {
+  index: resolveApp('public/index.html'),
+  config: resolveApp('public/config.html'),
+  video_overlay: resolveApp('public/video_overlay.html'),
+}
+
+const multipleEntry = require('react-app-rewire-multiple-entry')([
   {
-    entry: 'src/entry/bindex.tsx',
-    template: 'public/cappindex.html',
-    outPath: '/react/capp/index.html'
+    entry: resolveModule(resolveApp, `src/index`),
+    template: htmlFiles.index,
+    outPath: path.basename(htmlFiles.index)
+  },
+  {
+    entry: resolveModule(resolveApp, `src/config`),
+    template: htmlFiles.config,
+    outPath: path.basename(htmlFiles.config)
+  },
+  {
+    entry: resolveModule(resolveApp, `src/video_overlay`),
+    template: htmlFiles.video_overlay,
+    outPath: path.basename(htmlFiles.video_overlay)
   }
 ]);
 
-module.exports = {
-  webpack: function(config) {
-    multipleEntry.addMultiEntry(config);
-    return config;
-  }
-};
+module.exports = function override(config, webpackEnv) {
+  multipleEntry.addMultiEntry(config);
+
+  return config;
+}
---
+++ src/video_overlay.js
@@ -0,0 +1,3 @@
+console.log('video_overlay.tsx');
+
+export default {};
